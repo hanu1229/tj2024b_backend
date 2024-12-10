1c9d174fbc60d0924dd4e084441147d2c0cf7d4f
hanu1229 <han494127@gmail.com>
2024-12-10 16:35:44 +0900

    두번째 커밋

---
+++ tj2024b_backend/src/day02/Example5.java
@@ -0,0 +1,82 @@
+package day02;
+import java.util.Scanner;
+
+public class Example5 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		// JS day10연산자 문제 10개를 풀이하기
+		// [지문1] 국어 , 영어 , 수학 점수를 각 입력받아서 각 변수에 저장하고 총점 과 평균을 계산하여 console탭에 출력하시오.
+//		int kor, eng, math;
+//		System.out.print("국어 점수 : ");
+//		kor = scan.nextInt();
+//		System.out.print("영어 점수 : ");
+//		eng = scan.nextInt();
+//		System.out.print("수학 점수 : ");
+//		math = scan.nextInt();
+//		System.out.printf("총점 : %d, 평균 : %1.2f\n", (kor + eng + math), (float)(kor + eng + math)/3);
+      
+		// [지문2] 반지름를 입력받아서 원넓이[반지름*반지름*3.14] 계산하여 console탭에 출력하시오.
+//		int radius;
+//		System.out.print("반지름 : ");
+//		radius = scan.nextInt();
+//		System.out.printf("원의 넓이 : %.2f\n", radius*radius*3.14);
+      
+		// [지문3] 두 실수를 입력받아서 앞 실수의 값이 뒤실수의 값의 비율% 계산하여 console탭에 출력하시오.
+//		double value1, value2, result;
+//		System.out.print("실수1 : ");
+//		value1 = scan.nextDouble();
+//		System.out.print("실수2 : ");
+//		value2 = scan.nextDouble();
+//		result = (value1 / value2)*100;
+//		System.out.println("비율 : " + result + "%");
+      
+		// [지문4] 정수를 입력받아 입력받은 값이 홀수이면 true / 짝수이면 false 로 console탭에 출력하시오.
+//		int value;
+//		boolean result;
+//		System.out.print("홀/짝 : ");
+//		value = scan.nextInt();
+//		result = value % 2 == 0 ? false : true;
+//		System.out.println("result : " + result);
+      
+		// [지문5] 정수를 입력받아 입력받은 값이 7의 배수이면 true / 아니면 false 로 console탭에 출력하시오.
+//		int value;
+//		boolean result;
+//		System.out.print("정수 : ");
+//		value = scan.nextInt();
+//		result = value % 7 == 0 ? true : false;
+//		System.out.println("7의 배수 : " + result);
+		
+
+		// [지문6] 정수를 입력받아 입력받은 값이 홀수 이면서 7배수 이면 true / 아니면 false 로 console탭에 출력하시오.
+//		int value;
+//		boolean result;
+//		System.out.print("정수 : ");
+//		value = scan.nextInt();
+//		result = value % 2 == 1 ? (value % 7 == 0 ? true : false) : false;
+//		System.out.println("홀수이면서 7의 배수 : " + result);
+      
+		// [지문7] 십만원 단위의 금액을 입력받아 입력받은 금액의 지폐수를 console탭에 출력하시오.
+		// -> 입력 예] 356789 입력시 , 출력 예] 십만원:3장 만원:5장 천원6장
+//		int value, temp1, temp2, temp3;
+//		System.out.print("십만원 단위로 입력 : ");
+//		value = scan.nextInt();
+//		temp1 = value / 100000;
+//		temp2 = (value - (temp1 * 100000)) / 10000;
+//		temp3 = (value - (temp1 * 100000)-(temp2 * 10000)) / 1000;
+//		System.out.println("십만원 : " + temp1 + "장, 만원 : " + temp2 + "장, 천원 : " + temp3 + "장");
+      
+		// [지문8] 1차점수 와 2차점수 각각 입력받아서 
+        // -> 총점이 150점이상이면 '합격' 아니면 '불합격' 으로 console탭에 출력하시오.
+//		int value1, value2;
+//		String result;
+//		System.out.print("1차 점수 : ");
+//		value1 = scan.nextInt();
+//		System.out.print("2차 점수 : ");
+//		value2 = scan.nextInt();
+//		result = (value1 + value2) > 150 ? "합격" : "불합격";
+//		System.out.println("합격 여부 : " + result);
+		
+	}
+
+}
