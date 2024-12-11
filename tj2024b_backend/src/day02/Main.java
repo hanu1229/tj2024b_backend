8578abe2cb03cd23b6f1dd1199acc975e0588086
hanu1229 <han494127@gmail.com>
2024-12-11 18:21:02 +0900

    Day23 수업 끝

--- tj2024b_backend/src/day02/Main.java
+++ tj2024b_backend/src/day02/Main.java
@@ -1,15 +1,40 @@
 package day02;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
-	public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        short N = scan.nextShort();
-        String type = "";
-        for(int index = 0; index < N/4; index++) {
-        	type += "long ";
+	public static void main(String[] args) throws IOException {
+        //Scanner scan = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = "";
+        String[] arr = null;
+        int count = Integer.parseInt(br.readLine());
+        for(int index = 0; index < count; index++) {
+        	str = br.readLine();
+        	arr = str.split(" ");
+        	//System.out.println("arr[0] : " + arr[0] + " / " + "arr[1] : " + arr[1]);
+        	int num = (Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]));
+        	//System.out.println(Integer.parseInt(arr[0]) + " / " + Integer.parseInt(arr[1]));
+        	str = Integer.toString(num);
+        	//System.out.println(str);
+        	bw.write(str);
         }
-        type += "int";
-        System.out.println(type);
-        scan.close();
+        bw.flush();
+        //scan.close();
 	}
 }
+
+
+/*
+    Scanner scan = new Scanner(System.in);
+	while(scan.hasNextInt()) {
+		int A = scan.nextInt(), B = scan.nextInt();
+		System.out.println(A+B);
+	}
+	scan.close();
+*/
