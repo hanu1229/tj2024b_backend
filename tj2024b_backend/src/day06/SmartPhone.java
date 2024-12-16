39b034bf71356156b63b4ae827a8a0ddc19d2a32
hanu1229 <han494127@gmail.com>
2024-12-16 15:15:12 +0900

    Day06(Day26) 오후 수업 객체 생성

---
+++ tj2024b_backend/src/day06/SmartPhone.java
@@ -0,0 +1,11 @@
+package day06;
+
+public class SmartPhone {
+	String phoneNumber;
+	String phoneName;
+	String playTime;
+	int temperature;
+	String nowTime;
+	int battery;
+	boolean usimState;
+}
---
+++ tj2024b_backend/src/day06/Student.java
@@ -0,0 +1,8 @@
+package day06;
+
+public class Student {
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+}
