c7a4b0e592ce00af8d60dd6080a7abe708383f9d
hanu1229 <han494127@gmail.com>
2024-12-18 16:01:06 +0900

    Day08(Day28) 자바 오후 수업 끝

--- tj2024b_backend/src/day08/Student.java
+++ tj2024b_backend/src/day08/Student.java
@@ -13,4 +13,14 @@ public class Student {
 	public void setStudentName(String studentName) {
 		this.studentName = studentName;
 	}
+	
+	public static void main(String[] args) {
+		Student student = new Student();
+		student.studentName = "안연수";
+		Student student2 = new Student();
+		student2.setStudentName("안승연");
+		
+		System.out.println(student.getStudentName());
+		System.out.println(student2.getStudentName());
+	}
 }
