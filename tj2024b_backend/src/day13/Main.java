33c2a3e267d6b123548821420a8952f8b01ffca0
hanu1229 <han494127@gmail.com>
2024-12-27 14:47:39 +0900

    Day14(Day34) 오후 수업 과제로 자리 바꿈

--- tj2024b_backend/src/day13/Main.java
+++ tj2024b_backend/src/day13/Main.java
@@ -1,6 +1,6 @@
 package day13;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
@@ -8,24 +8,26 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int number = Integer.parseInt(st.nextToken());
-		int count = Integer.parseInt(st.nextToken());
-		int[] N = new int[number];
-		for(int index = 0; index < N.length; index++) {
-			N[index] = index+1;
+		int N = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		int high;
+		double total = 0;
+		st = new StringTokenizer(br.readLine());
+		for(int index = 0; index < arr.length; index++) {
+			arr[index] = Integer.parseInt(st.nextToken());
 		}
-		for(int index = 0; index < count; index++) {
-			st = new StringTokenizer(br.readLine());
-			int i = Integer.parseInt(st.nextToken())-1;
-			int j = Integer.parseInt(st.nextToken())-1;
-			for(int k = i; k <= j; k++, j--) {
-				int temp = N[k];
-				N[k] = N[j];
-				N[j] = temp;
+		// N = 3;
+		for(int index = 1; index < N; index++) {
+			if(arr[index-1] > arr[index]) {
+				int temp = arr[index-1];
+				arr[index-1] = arr[index];
+				arr[index] = temp;
 			}
 		}
-		for(int index = 0; index < N.length; index++) {
-			System.out.print(N[index] + " ");
+		high = arr[arr.length-1];
+		for(int index = 0; index < arr.length; index++) {
+			total += ((double)arr[index] / high) * 100;
 		}
+		System.out.println(total/N);
 	}
 }
