ee5e9c35924ce49f7586ce3624707e15287a0434
hanu1229 <han494127@gmail.com>
2024-12-31 18:04:51 +0900

    백준 푸는중

--- tj2024b_backend/src/day16/Main.java
+++ tj2024b_backend/src/day16/Main.java
@@ -3,30 +3,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int number = scan.nextInt();
-		for(int i = 1; i <= number; i++) {
-			for(int j = 1; j <= number - i; j++) {			
-				System.out.print(" ");
-			}
-			for(int j = 1; j <= 2 * i - 1; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = new StringTokenizer(br.readLine()).nextToken();
+		str = str.toUpperCase();
+		System.out.println(str);
+		System.out.println((int)str.charAt(0));
+		
+		for(int index = 0; index < str.length(); index++) {
+			
 		}
-		for(int i = number - 1; i >= 1; i--) {
-			for(int j = 1; j <= number - i; j++) {
-				System.out.print(" ");
-			}
-			for(int j = 1; j <= 2 * i - 1; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		scan.close();
 	}
 }
