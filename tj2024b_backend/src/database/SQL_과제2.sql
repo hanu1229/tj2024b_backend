7048f74150f26b260dbfc918715a46ea9c46cc21
hanu1229 <han494127@gmail.com>
2024-12-31 17:15:53 +0900

    Day16(Day36) 자바 상속 공부 및 SQL 과제 2

--- tj2024b_backend/src/database/SQL_과제2.sql
+++ tj2024b_backend/src/database/SQL_과제2.sql
@@ -12,6 +12,35 @@ create table membert(
     constraint primary key(mno)
 );
 
+# 카테고리 테이블 생성
+create table categoryt(
+	cno int auto_increment,
+    cname varchar(100) not null,
+    origin varchar(30) not null,
+    cdate datetime not null default now(),
+    constraint primary key(cno)
+);
+
+# 제품 테이블 생성
+create table productt(
+	pno int auto_increment,
+    pname varchar(100) not null,
+    prduct int unsigned default 0,
+    pcount int unsigned default 0,
+    constraint primary key(pno),
+    cno int,
+    constraint foreign key(cno) references categoryt(cno)
+);
+
+# 주문 테이블 생성
+create table ordert(
+	ono		int auto_increment	, # 주문 번호
+    odate	datetime not null default now() , # # 주문 날짜
+    arrival	datetime not null , # 배송 예상 날짜
+    today	bool not null default false,  # 당일 배송 논리
+    constraint primary key(ono)
+);
+
 # 주문상세 테이블 생성
 create table detailt (
 	dno int auto_increment,
@@ -24,3 +53,9 @@ create table detailt (
     constraint foreign key(pno) references productt(pno),
     constraint foreign key(mno) references membert(mno)
 );
+show tables;
+select * from membert;
+select * from categoryt;
+select * from productt;
+select * from ordert;
+select * from detailt;
